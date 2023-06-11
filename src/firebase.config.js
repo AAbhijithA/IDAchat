333b88dd054db8786bfd9cefb6ac154b25879ed0
Abhijith Ajith <113827354+AAbhijithA@users.noreply.github.com>
2023-06-11 21:12:50 +0530

    Add files via upload

---
+++ src/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB-Hcdg-h2EY6Ya-CwmtRtd0dxYogV-wG0",
+    authDomain: "idabackend.firebaseapp.com",
+    projectId: "idabackend",
+    storageBucket: "idabackend.appspot.com",
+    messagingSenderId: "834838422277",
+    appId: "1:834838422277:web:7e2a2ae705aa0cb0b70d6c"
+};
